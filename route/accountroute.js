543cfb88d08a11207b41f9e4512ba52e6e74c739
EhabMoazzam <EhabMoazzam@users.noreply.github.com>
2019-08-17 20:26:12 +0800

    init project

---
+++ route/accountroute.js
@@ -0,0 +1,7 @@
+module.exports = function(app, path){
+    app.get('/login', function(req, res){
+        let filepath = path.resolve('./www/login.html')
+
+        res.sendFile(filepath)
+    })
+}
